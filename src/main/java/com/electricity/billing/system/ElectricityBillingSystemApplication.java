53d028d5832d1a8b27aa739c1f462e5724fb31a6
Bineeth <bineethsagar333@gmail.com>
2023-11-28 18:06:43 +0530

    initial commit

---
+++ src/main/java/com/electricity/billing/system/ElectricityBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.electricity.billing.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBillingSystemApplication.class, args);
+	}
+
+}
